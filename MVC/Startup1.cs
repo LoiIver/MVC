5b995b31da4a3f3cf3ee5d1249e3ddf416fe6342
Anna Rysina <anna.d.rysina@gmail.com>
2016-03-31 20:43:35 +0300

    Mocha draft

---
+++ MVC/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MVC.Startup1))]
+
+namespace MVC
+{
+	public class Startup1
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+		}
+	}
+}
